b55a4d6c09e05105906d1142bf1af877e43917df
jordonsm <21117@bridgend.ac.uk>
2020-10-23 17:31:48 +0000

    Think this is right

--- src/exercise.py
+++ src/exercise.py
@@ -1,18 +1,18 @@
 def main():
     #write your code below this line
-    chicken_quantity = 3
+    chicken_quantity = 9000
     bacon_quantity = 5.5
     print("Chicken:")
     print(chicken_quantity)
     print("Bacon (kg):")
     print(bacon_quantity)
     print("Tractor:")
-    print("None!")
+    print("Zero")
     print()
     print("And finally, a summary:")
     print(chicken_quantity)
     print(bacon_quantity)
-    print("None!")
+    print("Zero")
 
 if __name__ == '__main__':
     main()
